bd872fad8ae495359b008e20fc225fca3f962647
homo-iocus <nils.grob@protonmail.com>
2020-08-05 16:26:23 +0200

    Solved Problem 7

---
+++ problem7.py
@@ -0,0 +1,6 @@
+from Prime import getPrimes
+
+primes = getPrimes(1000000)
+print("Primzahlen: {}".format(primes))
+print("ANz Primzahlen = {}".format(len(primes)))
+print("Solution: {}".format(primes[10000]))
