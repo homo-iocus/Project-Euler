9d31f46fc33a287c36054be54d978a830d429cc3
homo-iocus <nils.grob@protonmail.com>
2020-08-06 15:36:07 +0200

    Added method trial_division (Primfaktorzerlegung)

--- Prime.py
+++ Prime.py
@@ -34,3 +34,15 @@ def isPrime(n:int):
                 return False
 
     return True
+#Primfaktorzerlegung 
+def trial_division(n: int) -> list:
+    # Return a list of the prime factors for a natural number.
+    a = []               # Prepare an empty list.
+    f = 2                # The first possible factor.
+    while n > 1:         # While n still has remaining factors...
+        if n % f == 0:   # The remainder of n divided by f might be zero.
+            a.append(f)  # If so, it divides n. Add f to the list.
+            n /= f       # Divide that factor out of n.
+        else:            # But if f is not a factor of n,
+            f = f + 1       # Add one to f and try again.
+    return a             # Prime factors may be repeated: 12 factors to
