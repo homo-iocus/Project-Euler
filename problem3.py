5d0308988facdd8d98fb5e11b214f1c361a3bc7d
homo-iocus <nils.grob@protonmail.com>
2020-08-06 16:47:56 +0200

    Added own Solution to Problem 3

--- problem3.py
+++ problem3.py
@@ -1,17 +1,15 @@
 #Problem 3 : https://projecteuler.net/problem=3
 
-# solution from forum by seraph76
+def trial_division(n: int) -> list:
+    # Return a list of the prime factors for a natural number.
+    a = []
+    f = 2                # The first possible factor.
+    while n > 1:         # While n still has remaining factors...
+        if n % f == 0:   # The remainder of n divided by f might be zero.
+            a.append(f)  # If so, it divides n. Add f to the list.
+            n /= f       # Divide that factor out of n.
+        else:            # But if f is not a factor of n,
+            f = f + 1       # Add one to f and try again.
+    return a
 
-def factors(param):
-    i = 2
-    factors = []
-    while i <= param:
-        if (param % i) == 0:
-            factors.append(i)
-            param = param / i
-        i += 1
-    return factors
-
-
-
-print(factors(600851475143))
+print(trial_division(600851475143))
