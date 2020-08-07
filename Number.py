30ecb16ca0d80218872c0a3affde8d74c4dd5e6a
homo-iocus <nils.grob@protonmail.com>
2020-08-07 17:30:54 +0200

    Added method fib (fibannoci)

--- Number.py
+++ Number.py
@@ -12,3 +12,12 @@ def ggT(a:int, b: int) -> int:
         a = b;
         b = h;
     return a
+
+def fib(n: int) -> int:
+    if n == 0:
+        return n
+    last = 0
+    next = 1
+    for _ in range(1, n):
+        last, next = next, last+next
+    return next
